44da471158fdc2a87070e864a1d939999fa39353
Lorenzo Libron <lblibron@unionbankph.com>
2022-12-08 14:30:17 +0800

    add footer, apply login page responsiveness

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,32 +1,27 @@
 import "./style.css";
 import { Formik, Form } from "formik";
 import { Link } from "react-router-dom";
+import * as Yup from "yup";
 import LoginInput from "../../components/inputs/loginInput";
 import { useState } from "react";
-import * as Yup from "yup";
-
 const loginInfos = {
   email: "",
   password: "",
 };
-
 export default function Login() {
   const [login, setLogin] = useState(loginInfos);
   const { email, password } = login;
-
   const handleLoginChange = (e) => {
     const { name, value } = e.target;
     setLogin({ ...login, [name]: value });
   };
-
   const loginValidation = Yup.object({
     email: Yup.string()
-      .required("Email address is required!")
-      .email("Must be a valid email")
+      .required("Email address is required.")
+      .email("Must be a valid email.")
       .max(100),
     password: Yup.string().required("Password is required"),
   });
-
   return (
     <div className="login">
       <div className="login_wrapper">
@@ -41,7 +36,10 @@ export default function Login() {
             <div className="login_2_wrap">
               <Formik
                 enableReinitialize
-                initialValues={{ email, password }}
+                initialValues={{
+                  email,
+                  password,
+                }}
                 validationSchema={loginValidation}
               >
                 {(formik) => (
@@ -49,7 +47,7 @@ export default function Login() {
                     <LoginInput
                       type="text"
                       name="email"
-                      placeholder="Email address or Phone number"
+                      placeholder="Email address or phone number"
                       onChange={handleLoginChange}
                     />
                     <LoginInput
@@ -72,12 +70,68 @@ export default function Login() {
               <button className="blue_btn open_signup">Create Account</button>
             </div>
             <Link to="/" className="sign_extra">
-              <b>Create a Page</b>
-              for a celebrity, brand or business.
+              <b>Create a Page</b> for a celebrity, brand or business.
             </Link>
           </div>
         </div>
         <div className="register"></div>
+        <footer className="login_footer">
+          <div className="login_footer_wrap">
+            <Link to="/">English(UK)</Link>
+            <Link to="/">Français(FR)</Link>
+            <Link to="/">العربية</Link>
+            <Link to="/">ⵜⴰⵎⴰⵣⵉⵖⵜ</Link>
+            <Link to="/">Español (España)</Link>
+            <Link to="/">italiano</Link>
+            <Link to="/">Deutsch</Link>
+            <Link to="/">Português (Brasil)</Link>
+            <Link to="/">हिन्दी</Link>
+            <Link to="/">中文(简体)</Link>
+            <Link to="/">日本語</Link>
+            <Link to="/" className="footer_square">
+              <i className="plus_icon"></i>
+            </Link>
+          </div>
+          <div className="footer_splitter"></div>
+          <div className="login_footer_wrap">
+            <Link to="/">Sign Up</Link>
+            <Link to="/">Log in</Link>
+            <Link to="/">Messenger</Link>
+            <Link to="/">Facebook Lite</Link>
+            <Link to="/">Watch</Link>
+            <Link to="/">Places</Link>
+            <Link to="/">Games</Link>
+            <Link to="/">Marketplace</Link>
+            <Link to="/">Facebook Pay</Link>
+            <Link to="/">Oculus</Link>
+            <Link to="/">Portal</Link>
+            <Link to="/">Instagram</Link>
+            <Link to="/">Bulletin</Link>
+            <Link to="/">Local</Link>
+            <Link to="/">Fundraisers</Link>
+            <Link to="/">Services</Link>
+            <Link to="/">Voting Information Centre</Link>
+            <Link to="/">Groups</Link>
+            <Link to="/">About</Link>
+            <Link to="/">Create ad</Link>
+            <Link to="/">Create Page</Link>
+            <Link to="/">Developers</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Privacy</Link>
+            <Link to="/">Cookies</Link>
+            <Link to="/">
+              AdChoices
+              <i className="adChoices_icon"></i>
+            </Link>
+            <Link to="/">Terms</Link>
+            <Link to="/">Help</Link>
+          </div>
+          <div className="login_footer_wrap">
+            <Link to="/" style={{ fontSize: "12px", marginTop: "10px" }}>
+              Meta © 2022
+            </Link>
+          </div>
+        </footer>
       </div>
     </div>
   );
